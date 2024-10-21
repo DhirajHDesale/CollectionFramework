1e9dbd5f4669a409519f667c18062499b71beddf
DhirajHDesale <dhirajhanumantpatil@gmail.ocm>
2024-10-21 18:19:50 +0530

    Stack Class

---
+++ List/Stack1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Stack;
+public class Stack1 {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println(stack.isEmpty());
+        stack.clear();
+        System.out.println(stack);
+
+
+       
+    }
+}
